bb0585eb5d059412d1d420f81e25f03029efb14f
Mark Taratynov <mark.taratynov@gmail.com>
2024-11-04 20:26:12 +0100

    Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Firstsection from "./components/Main/Firstsection/Firstsection";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Firstsection />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+const Footer = () => (
+  <footer>
+    <nav className="navigation-box">
+      <a href="#">Главная</a>
+      <a href="#">О нас</a>
+      <a href="#">Контакты</a>
+    </nav>
+    <div className="footer-container">
+      <p>&copy; 2024 Помогатор-112. Все права защищены.</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
